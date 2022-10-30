c0fc0415ec619f5b850ccd691f3e14af1cc06cf2
mathias852 <mathias.engmark@gmail.com>
2022-10-30 13:13:25 +0800

    Added 5 more screens
    Still missing the last tripoverview screen

--- app/src/main/java/com/example/madmleproject/MainActivity.java
+++ app/src/main/java/com/example/madmleproject/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.location_profile);
+        setContentView(R.layout.trip_overview);
     }
 }
