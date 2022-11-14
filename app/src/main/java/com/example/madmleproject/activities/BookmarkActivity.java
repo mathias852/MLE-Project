ea0d96af3e0eca5e6b86a785b6274c810b6d6fef
mathias852 <mathias.engmark@gmail.com>
2022-11-14 13:51:53 +0800

    Fixed dynamic updating

--- app/src/main/java/com/example/madmleproject/activities/BookmarkActivity.java
+++ app/src/main/java/com/example/madmleproject/activities/BookmarkActivity.java
@@ -2,14 +2,10 @@ package com.example.madmleproject.activities;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,8 +20,6 @@ public class BookmarkActivity extends AppCompatActivity {
 
     ImageView bookmarkIconPenangHill, bookmarkIconPenangHillBookmarked;
 
-
-
     BookmarksRepo bookmarksRepo = RepoManager.getRepoManager().getBookmarksRepo();
 
     @SuppressLint("CutPasteId")
@@ -39,32 +33,34 @@ public class BookmarkActivity extends AppCompatActivity {
         InformationPriceRange = findViewById(R.id.InformationPrice_range);
         bookmarkTitle = findViewById(R.id.bookmarkTitle);
 
-        //Name of Landmarks
+        //Name of Bookmark
         penang_hill = findViewById(R.id.bookmarkTitle);
 
         //Bookmark Icons
         bookmarkIconPenangHill = findViewById(R.id.bookmarkIconPenangHill);
-
         bookmarkIconPenangHillBookmarked = findViewById(R.id.bookmarkIconPenangHillBookmarked);
+        //Update
+        updateVisibilityPenangHill();
     }
 
 
-    public void updateBookmark(View view){
+    public void updateBookmark(View view) {
         String landmarkName = view.getContentDescription().toString();
         bookmarksRepo.updateBookmarkLandmark(landmarkName);
 
-        switch (landmarkName){
-            case("Penang Hill"):
+        switch (landmarkName) {
+            case ("Penang Hill"):
                 updateVisibilityPenangHill();
                 break;
         }
     }
-    private void updateVisibilityPenangHill(){
+
+    private void updateVisibilityPenangHill() {
         String landmarkName = penang_hill.getText().toString();
-        if(bookmarksRepo.isBookmarked(RepoManager.getRepoManager().getLandmarksRepo().getLandmarkIdFromName(landmarkName))){
+        if (bookmarksRepo.isBookmarked(RepoManager.getRepoManager().getLandmarksRepo().getLandmarkIdFromName(landmarkName))) {
             bookmarkIconPenangHill.setVisibility(View.INVISIBLE);
             bookmarkIconPenangHillBookmarked.setVisibility(View.VISIBLE);
-        } else{
+        } else {
             bookmarkIconPenangHill.setVisibility(View.VISIBLE);
             bookmarkIconPenangHillBookmarked.setVisibility(View.INVISIBLE);
         }
@@ -76,7 +72,6 @@ public class BookmarkActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
-
 
 
     public void goToSearch(View view) {
